6ee1cd4de27c6e451a87974f33f7e1e6ba50bc65
driftkiller <giovannidann@hotmail.com>
2019-07-29 01:46:17 -0400

    Zalcano plugin
    Shows AOE for bosses and when to run from the glowing rocks and more.
    
    Still have to improve the step system telling users to mine/smelt so it's a hidden option for now.

--- runelite-api/src/main/java/net/runelite/api/ProjectileID.java
+++ runelite-api/src/main/java/net/runelite/api/ProjectileID.java
@@ -81,6 +81,8 @@ public class ProjectileID
 	public static final int XARPUS_ACID = 1555;
 	public static final int CERB_FIRE = 1247;
 
+	public static final int ZALCANO_PROJECTILE = 1728;
+
 	/**
 	 * missing: superior dark beast
 	 */
